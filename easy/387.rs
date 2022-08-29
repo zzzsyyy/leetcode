e0f7e74c9eceef70716282e3ee541bc2625684c3
zzzsyyy <me@zzzsy.top>
2022-08-29 19:12:30 +0800

    add: 7 387

---
+++ easy/387.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn first_uniq_char(s: String) -> i32 {
+        let mut cs = HashMap::new();
+        s.chars().for_each(|c| *cs.entry(c).or_insert(0) += 1);
+        for (i, c) in s.chars().enumerate() {
+            if cs[&c] == 1 {
+                return i as i32;
+            }
+        }
+        -1
+    }
+}
