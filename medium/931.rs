0f39c055629f366efee15ab8dc481971ecc998cf
zzzsyyy <me@zzzsy.top>
2022-10-10 16:35:29 +0800

    update: 931

--- medium/931.rs
+++ medium/931.rs
@@ -1,24 +1,20 @@
-fn min_falling_path_sum(matrix: Vec<Vec<i32>>) -> i32 {
-    let n = matrix.len();
-    let mut dp = vec![vec![0; n]; 2];
-    dp[0] = matrix[0].clone();
-    for i in 1..n {
-        let v = i & 1;
-        for j in 0..n {
-            if j == 0 {
-                dp[v][j] = dp[1 - v][j].min(dp[1 - v][j + 1]) + matrix[i][j];
-            } else if j == n - 1 {
-                dp[v][j] = dp[1 - v][j].min(dp[1 - v][j - 1]) + matrix[i][j];
-            } else {
-                dp[v][j] = dp[1 - v][j].min(dp[1 - v][j + 1]).min(dp[1 - v][j - 1]) + matrix[i][j];
+impl Solution {
+    pub fn min_falling_path_sum(matrix: Vec<Vec<i32>>) -> i32 {
+        let n = matrix.len();
+        let mut dp = vec![vec![0; n]; 2];
+        dp[0] = matrix[0].clone();
+        for (i, v) in matrix.iter().skip(1).enumerate() {
+            let t = 1 - i & 1;
+            for (j, w) in v.iter().enumerate() {
+                if j == 0 {
+                    dp[t][j] = dp[1 - t][j].min(dp[1 - t][j + 1]) + w;
+                } else if j == n - 1 {
+                    dp[t][j] = dp[1 - t][j].min(dp[1 - t][j - 1]) + w;
+                } else {
+                    dp[t][j] = dp[1 - t][j].min(dp[1 - t][j + 1]).min(dp[1 - t][j - 1]) + w;
+                }
             }
         }
+        *dp[(n - 1) & 1].iter().min().unwrap()
     }
-    *dp[(n - 1) & 1].iter().min().unwrap()
-}
-
-fn main() {
-    let m = [[2, 1, 3].to_vec(), [6, 5, 4].to_vec(), [7, 8, 9].to_vec()].to_vec();
-    let ret = min_falling_path_sum(m);
-    println!("{}", ret);
 }
