21d13f8b3b3adca2635195ee67285ffd448a16f9
zzzsyyy <me@zzzsy.top>
2022-10-11 18:56:51 +0800

    update: 1289

--- hard/1289.rs
+++ hard/1289.rs
@@ -1,43 +1,31 @@
-macro_rules! vecnd {
-    ($([$($inner:tt)*]),+ $(,)?) => {
-        vec![$(
-            vecnd![$($inner)*]
-        ),+]
-    };
-    ($($t:tt)*) => {
-        vec![$($t)*]
-    };
-}
-
-fn min_falling_path_sum(grid: Vec<Vec<i32>>) -> i32 {
-    let n = grid.len();
-    let mut dp = vec![vec![0; n]; 2];
-    dp[0] = grid[0].clone();
-    for i in 1..n {
-        let v = i & 1;
-        for j in 0..n {
-            dp[v][j] = grid[i][j]
-                + dp[1 - v]
-                    .iter()
-                    .enumerate()
-                    .filter(|(k, v)| *k != j)
-                    .map(|(_, e)| e)
-                    .min()
-                    .unwrap();
+impl Solution {
+    pub fn min_falling_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+        let n = grid.len();
+        let mut dp = vec![vec![0; n]; 2];
+        dp[0] = grid[0].clone();
+        for (i,v) in grid.iter().skip(1).enumerate() {
+            let t = 1-i & 1;
+            let (idx, val) = dp[1 - t]
+                .clone()
+                .into_iter()
+                .enumerate()
+                .min_by(|a, b| a.1.cmp(&b.1))
+                .unwrap();
+            for (j, w) in v.iter().enumerate() {
+                if j != idx {
+                    dp[v][j] = val + grid[i][j];
+                } else {
+                    dp[v][j] = grid[i][j]
+                        + dp[1 - v]
+                            .iter()
+                            .enumerate()
+                            .filter(|(k, _)| *k != j)
+                            .map(|(_, e)| e)
+                            .min()
+                            .unwrap();
+                }
+            }
         }
+        *dp[(n - 1) & 1].iter().min().unwrap()
     }
-    *dp[(n - 1) & 1].iter().min().unwrap()
-}
-
-fn main() {
-    // let m = [[2, 1, 3].to_vec(), [6, 5, 4].to_vec(), [7, 8, 9].to_vec()].to_vec();
-    let g = vecnd![
-        [-73, 61, 43, -48, -36],
-        [3, 30, 27, 57, 10],
-        [96, -76, 84, 59, -15],
-        [5, -49, 76, 31, -7],
-        [97, 91, 61, -46, 67]
-    ];
-    let ret = min_falling_path_sum(g);
-    println!("{}", ret);
 }
