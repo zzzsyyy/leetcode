6f41b658107de839959d3774dc1172deca8a64d7
zzzsyyy <me@zzzsy.top>
2022-10-21 13:51:18 +0800

    add: 901

---
+++ medium/901.rs
@@ -0,0 +1,25 @@
+struct StockSpanner {
+    prices: Vec<i32>,
+}
+
+impl StockSpanner {
+    fn new() -> Self {
+        StockSpanner { prices: Vec::new() }
+    }
+
+    fn next(&mut self, price: i32) -> i32 {
+        self.prices.push(price);
+
+        let mut size = 0;
+
+        for v in self.prices.iter().rev() {
+            if *v <= price {
+                size += 1;
+            } else {
+                break;
+            }
+        }
+
+        size
+    }
+}
